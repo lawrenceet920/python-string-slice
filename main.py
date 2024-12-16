997a0152890744d2385660e5b029b1bc87156622
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-12-16 13:17:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Matthew Hall
+# Ethan Lawrence
+# Dec 16 2024
+# slicng strings
+
+# To slice a string you use string notation
+var = 'I am a string!'
+print(var[2:4])
+
+# The string notation is EXCLUSIVE so 2 is not read (4 is)
+# It also is based on index numbers (0 1 2)
+var = '0123456789'
+print(var[2:-4])
